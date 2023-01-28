ecfe8a554dea73e8c4bea03c9ad438a1c7072bd5
amirhossein-muhammadi <123809063+amirhossein-muhammadi@users.noreply.github.com>
2023-01-28 14:35:40 +0330

    Add files via upload

--- RPi/GPIO.py
+++ RPi/GPIO.py
@@ -1,1 +1,25 @@
- #
+
+import random
+BOARD = 1
+OUT = 1
+IN = 1
+
+def setmode(a):
+   print (a)
+def setup(a, b):
+   print (a)
+def output(a, b):
+   print (a)
+
+def output(a, b):
+   print (a)
+   return random.choice([0,1])
+
+def input(a):
+   print (a)
+   return random.choice([0,1])
+   
+def cleanup():
+   print ('a')
+def setwarnings(flag):
+   print ('False')
