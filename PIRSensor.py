0526b80c03688972225f1433024830f1e4915cec
amirhossein-muhammadi <123809063+amirhossein-muhammadi@users.noreply.github.com>
2023-02-01 16:00:27 +0330

    Update PIRSensor.py

--- PIRSensor.py
+++ PIRSensor.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
+GPIO.setwarnings(False)         # GPIO is faking by making a module in the root
+                                # When connected to rapsberry we can use gpio of pie
 #GPIO.s(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
@@ -8,19 +9,18 @@ import urllib.request
 
 
 #this function return 1 if the PIR Sensor has a motion, return 0 if no motion
-def readPIR(delay=1):
+def readPIR():
     
     
     i=GPIO.input(11)
     if i==0:                 #When output from motion sensor is LOW
-        print ("No intruders"),i
-        time.sleep(.1)
-
+        print ("No intruders",i)
+        return i
+        
     elif i==1:               #When output from motion sensor is HIGH
-        print ("Intruder detected"),i
-        time.sleep(delay)
+        print ("Intruder detected",i)
+        return i
 
-    return i
 
 
 def checkConnection(host='http://google.com'):
